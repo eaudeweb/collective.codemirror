858f789a5f670891865c7736bcf44111b7521554
Alex Morega <alex@grep.ro>
2012-03-16 11:00:06 +0100

    clean up script code to match template code

--- src/collective/codemirror/__init__.py
+++ src/collective/codemirror/__init__.py
@@ -9,6 +9,19 @@ def initialize(context):
     patch_pythonscripts()
     patch_pagetemplates()
 
+def _cursor_position(request):
+    cursor_position = request.get('codemirror-cursor-position', None)
+    if cursor_position:
+        return {
+            'cursor_position': True,
+            'line': int(cursor_position.split('-')[0]),
+            'ch': int(cursor_position.split('-')[1]),
+        }
+    else:
+        return {
+            'cursor_position': False,
+        }
+
 def patch_pythonscripts():
     import Products.PythonScripts
     PythonScript = Products.PythonScripts.PythonScript.PythonScript
@@ -16,13 +29,15 @@ def patch_pythonscripts():
     PythonScript.manage = PythonScript.manage_main = ZPythonScriptHTML_editForm
     PythonScript.ZPythonScriptHTML_editForm =ZPythonScriptHTML_editForm
     original_compile = PythonScript._compile
-    def get_codemirror_json(self):
+    def get_codemirror_json(self, request):
         error_lines = [int(re.sub(r'.*line ([0-9]+)\).*',r'\1',error))
                        for error in self.errors
                        if re.match(r'.*line ([0-9]+)\).*', error)]
-        return json.dumps({
+        data = {
             'error_lines': error_lines,
-        })
+        }
+        data.update(_cursor_position(request))
+        return json.dumps(data)
     PythonScript.get_codemirror_json = get_codemirror_json
 
 def patch_pagetemplates():
@@ -39,19 +54,10 @@ def patch_pagetemplates():
         error_lines = [int(re.sub(r'.*line ([0-9]+),.*',r'\1',error))
                        for error in getattr(self, '_v_errors', [])
                        if re.match(r'.*line ([0-9]+),.*', error)]
-        cursor_position = request.get('codemirror-cursor-position', None)
         data = {
             'error_lines': error_lines,
-            'cursor_position': bool(cursor_position),
         }
-        if cursor_position:
-            data.update({
-                'cursor_position': True,
-                'line': int(cursor_position.split('-')[0]),
-                'ch': int(cursor_position.split('-')[1]),
-            })
-        else:
-            data['cursor_position'] = False
+        data.update(_cursor_position(request))
         return json.dumps(data)
     ZopePageTemplate.get_codemirror_json = get_codemirror_json
     original_read = ZopePageTemplate.read
